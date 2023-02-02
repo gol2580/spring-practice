806e3db068d618b4de9917bfab8ace2acb2de276
jieun_Kim <gol11971197@gmail.com>
2023-02-02 17:57:48 +0900

    create project

---
+++ src/main/java/com/project/partyMatching/PartyMatchingApplication.java
@@ -0,0 +1,13 @@
+package com.project.partyMatching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartyMatchingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartyMatchingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/partyMatching/PartyMatchingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.partyMatching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartyMatchingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
